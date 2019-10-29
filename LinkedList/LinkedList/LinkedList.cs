8e464abb142434e9275e496ccbf381ec5b16b73d
Satish Alturu <satish.alturu@elliemae.com>
2019-10-29 09:21:35 -0700

    Initial version

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+    }
+
+    public class List
+    {
+        public Node Head { get; set; }
+
+        public List()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            if (Head == null)
+            {
+                Head = new Node(data);
+            }
+            else
+            {
+                Node temp = Head;
+                while (temp.Next != null)
+                    temp = temp.Next;
+                temp.Next = new Node(data);
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = Head;
+            while (temp.Next != null)
+            {
+                Console.Write(temp.Data + "-->");
+                temp = temp.Next;
+            }
+            Console.Write(temp.Data + "\n");
+        }
+
+        public void Display(Node node)
+        {
+            while (node.Next != null)
+            {
+                Console.Write(node.Data + "-->");
+                node = node.Next;
+            }
+            Console.Write(node.Data + "\n");
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create linked list
+            List list = new List();
+            list.AddNode(10);
+            list.AddNode(20);
+            list.AddNode(30);
+            list.AddNode(40);
+            list.AddNode(50);
+
+            Console.WriteLine("1. Linked List : \t");
+            list.Display();
+        }
+    }
+}
